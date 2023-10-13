4eedd7936b496898e866aa7710adf0a08725828b
Keven Pacheco <kevenpacheco2000@gmail.com>
2023-10-13 13:21:28 -0300

    Class 02 - Node

---
+++ nodejs/aulas/02-api-rest/src/database.ts
@@ -0,0 +1,16 @@
+import { knex as knexSetup, Knex } from 'knex'
+import { env } from './env'
+
+export const config: Knex.Config = {
+  client: 'sqlite3',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  migrations: {
+    directory: './db/migrations',
+    extension: 'ts',
+  },
+  useNullAsDefault: true,
+}
+
+export const knex = knexSetup(config)
